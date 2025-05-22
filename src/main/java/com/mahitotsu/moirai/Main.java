22e404a4763189dd7df695196cfe5c350b204b94
Akio Kanashiki <akio@mail.com>
2025-05-22 11:35:16 +0900

    modified

--- src/main/java/com/mahitotsu/moirai/Main.java
+++ src/main/java/com/mahitotsu/moirai/Main.java
@@ -2,6 +2,7 @@ package com.mahitotsu.moirai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import software.amazon.awssdk.services.bedrockagentruntime.BedrockAgentRuntimeAsyncClient;
 
@@ -12,6 +13,7 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    @Bean
     public BedrockAgentRuntimeAsyncClient bedrockAgentRuntimeAsyncClient() {
         return BedrockAgentRuntimeAsyncClient.create();
     }
